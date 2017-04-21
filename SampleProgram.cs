a9ecb4dcf52c2fb26094d9b08908c6efbcb03a87
Gokul Rajagopal <Gokul Rajagopal>
2017-04-21 21:03:10 +0530

    Added a new sample program for testing

---
+++ SampleProgram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Class1
+    {
+        public static void main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
