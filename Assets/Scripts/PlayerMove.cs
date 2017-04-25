32378ac6b8c4cbda4c89669bd726d314c4ad910d
Gokul Rajagopal <Gokul Rajagopal>
2017-04-25 23:24:36 +0530

    Added new scene and expermenting with new sprites and scripts. (Also included standard assets)

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+	Rigidbody2D rb;
+	public float moveSpeed=5f;
+	private bool ifClicked;
+	private int stairsPassed=1;
+	
+	// Use this for initialization
+	void Start() 
+	{
+		rb = GetComponent<Rigidbody2D>();
+		rb.velocity = new Vector2(moveSpeed, 0);
+	}
+	
+	void OnTriggerEnter2D(Collider2D cols)
+	{
+		if(cols.gameObject.tag == "Edge" && ifClicked == true)
+		{
+			Debug.Log("Turn!");
+			GameObject.Find("Stair (" + stairsPassed + ")").GetComponent<EdgeCollider2D>().enabled = false;
+			moveSpeed = -Mathf.Abs(moveSpeed);
+			Debug.Log(moveSpeed);
+			rb.velocity = new Vector2(moveSpeed, 0);
+			stairsPassed++;
+		}
+	}
+	
+	IEnumerator OnMouseDown()
+	{
+		Debug.Log("Clicked!");
+		ifClicked = true;
+		yield return new WaitForSeconds(1f);
+		ifClicked = false;
+	}
+}
